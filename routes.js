a6e20d0965691cac9f84b995419e5fa02fa400cb
Bruno do Carmo Dev <126525394+BrunoCarmoS2004@users.noreply.github.com>
2024-08-30 21:10:46 -0300

    Finalizado a atividade
    Post via url feito

--- routes.js
+++ routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const enderecoController = require('./controllers/EnderecoController');
+const enderecoController = require('./controller/EnderecoController');
 
 const router = express.Router();
 
-router.post('/enderecos', enderecoController.createEndereco);
+router.post('/enderecos/:cep', enderecoController.createEndereco);
 router.get('/enderecos', enderecoController.getAllEnderecos);
 router.get('/enderecos/:Id', enderecoController.getEnderecoById);
 router.put('/enderecos/:Id', enderecoController.updateEndereco);
